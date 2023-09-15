c0daf3fe97bef250c76b0ede041fa4badab78642
flo_flooff <floflo.off@gmail.com>
2023-09-15 16:49:51 +0200

    Configuration de la BDD et autre inforamtions

--- configs.php
+++ configs.php
@@ -1,12 +1,12 @@
 <?php
 
 $DB_SERVER = getenv("MVC_SERVER") ?: "127.0.0.1";
-$DB_DATABASE = getenv("MVC_DB") ?: "mediatheque";
-$DB_USER = getenv("MVC_USER") ?: "root";
-$DB_PASSWORD = getenv("MVC_TOKEN") ?: "root";
+$DB_DATABASE = getenv("MVC_DB") ?: "ap_3_mediatout";
+$DB_USER = getenv("MVC_USER") ?: "WEB";
+$DB_PASSWORD = getenv("MVC_TOKEN") ?: "g&xoobrNE%m6C@sW";
 $DEBUG = getenv("MVC_DEBUG") ?: true;
-$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "http://localhost:9000/valider-compte/";
-$MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "localhost";
+$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "http://mediatout.florianjaunet.fr/valider-compte/";
+$MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "192.168.10.15";
 $FROM_EMAIL = getenv("MVC_FROM_EMAIL") ?: "contact@localhost.fr";
 
 return array(
