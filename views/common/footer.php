399ad2ca9fc94bff19b1344f2891dfb74e9cf44b
flo_flooff <floflo.off@gmail.com>
2023-10-19 09:20:02 +0200

    Retrait de l'accès developpeur pour le moment, pas de gestion de l'affichage des données

--- views/common/footer.php
+++ views/common/footer.php
@@ -3,7 +3,7 @@
     <p class="text-white">
         © 2023 Médiathèque. Tous droits réservés.
         -
-        <a href="/api" class="text-white hover:underline">Accès développeur</a>
+        <!-- <a href="/api" class="text-white hover:underline">Accès développeur</a> -->
     </p>
 </footer>
 </body>
