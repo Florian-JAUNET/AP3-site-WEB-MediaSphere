8ff91a698c1eb8e0f0358f33f48bd6b06aa9ad5a
flo_flooff <floflo.off@gmail.com>
2023-10-20 15:05:56 +0200

    Modification mineure pour test et sécurité

--- configs.php
+++ configs.php
@@ -8,7 +8,7 @@ $DB_USER = getenv("MVC_USER") ?: "WEB";
 // $DB_PASSWORD = getenv("MVC_TOKEN") ?: "";
 $DB_PASSWORD = getenv("MVC_TOKEN") ?: "g&xoobrNE%m6C@sW";
 $DEBUG = getenv("MVC_DEBUG") ?: true;
-$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "http://mediatout.florianjaunet.fr/valider-compte/";
+$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "http://localhost:9000/valider-compte/";
 $MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "192.168.10.15";
 $FROM_EMAIL = getenv("MVC_FROM_EMAIL") ?: "contact@mediatout.fr";
 
@@ -21,3 +21,5 @@ return array(
     "FROM_EMAIL" => $FROM_EMAIL,
     "URL_VALIDATION" => $URL_VALIDATION
 );
+
+?>
