dec9deb164fcd8ad69389d2f09d58598fcf2f2f0
flo_flooff <floflo.off@gmail.com>
2023-11-23 23:14:00 +0100

    [CONTROLLER] Ajout de la modification d'utilisateur et de téléchargement des informations utilisateurs

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -25,6 +25,7 @@ class UserController extends WebController
      * Déconnecte l'utilisateur.
      * @return string
      */
+
     function logout(): string
     {
         SessionHelpers::logout();
@@ -37,6 +38,7 @@ class UserController extends WebController
      * Si la connexion échoue, un message d'erreur est affiché.
      * @return string
      */
+
     function login(): string
     {
         $data = array();
@@ -70,6 +72,7 @@ class UserController extends WebController
      * Si l'inscription échoue, un message d'erreur est affiché.
      * @return string
      */
+
     function signup(): string
     {
         $data = array();
@@ -78,7 +81,6 @@ class UserController extends WebController
         if (SessionHelpers::isConnected()) {
             return $this->redirect("/me");
         }
-
 
         // Gestion de l'inscription
         if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["telephone"])) {
@@ -97,13 +99,14 @@ class UserController extends WebController
         return Template::render("views/user/signup.php", $data);
     }
 
-    function signupValidate($uuid){
+    function signupValidate($uuid)
+    {
         $result = $this->emprunteur->validateAccount($uuid);
 
-        if($result){
+        if ($result) {
             // Affichage de la page de finalisation de l'inscription
             return Template::render("views/user/signup-validate-success.php");
-        }else{
+        } else {
             // Gestion d'erreur à améliorer
             return parent::redirect("/");
         }
@@ -114,6 +117,7 @@ class UserController extends WebController
      * Si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion.
      * @return string
      */
+
     function me(): string
     {
         // Récupération de l'utilisateur connecté en SESSION.
@@ -132,31 +136,88 @@ class UserController extends WebController
      * Pour accéder à la page il faut également l'id de la ressource et l'id de l'exemplaire.
      * @return string
      */
-    function emprunter(): string
+
+    function emprunter(int $id): string
     {
-        // Id à emprunter
+        // Récupération de l'id de la ressource et de l'id de l'exemplaire
         $idRessource = $_POST["idRessource"];
         $idExemplaire = $_POST["idExemplaire"];
 
         // Récupération de l'utilisateur connecté en SESSION.
         $user = SessionHelpers::getConnected();
 
-        if (!$user || !$idRessource || !$idExemplaire) {
-            // Gestion d'erreur à améliorer
-            die ("Erreur: utilisateur non connecté ou ids non renseignés");
-        }
+        // Vérication si l'utilisateur a plus de 3 emprunts en cours
+        $emprunts = $this->emprunter->getEmprunts($user->idemprunteur);
 
         // On déclare l'emprunt, et on redirige l'utilisateur vers sa page de profil
         $result = $this->emprunter->declarerEmprunt($idRessource, $idExemplaire, $user->idemprunteur);
 
         if ($result) {
             // Envoi d'un email de confirmation
-            EmailUtils::sendEmail($user->emailemprunteur, "Confirmation d'emprunt", "emprunt", array("user" => $user));
+            EmailUtils::sendEmail($user->emailemprunteur, "Confirmation d'emprunt", "emprunt", array("user" => $user, "emprunts" => $emprunts));
+            $this->redirect("/me");
+        } elseif (count($emprunts) >= 3) {
+            // Envoi d'un email d'erreur
+            EmailUtils::sendEmail($user->emailemprunteur, "Erreur lors de l'emprunt", "erreurEmpruntNombre", array("user" => $user, "emprunts" => $emprunts));
             $this->redirect("/me");
         } else {
-            // Gestion d'erreur à améliorer
-            die("Erreur lors de l'emprunt");
+            // Envoi d'un email d'erreur
+            EmailUtils::sendEmail($user->emailemprunteur, "Erreur lors de l'emprunt", "erreurEmprunt", array("user" => $user, "emprunts" => $emprunts));
+            $this->redirect("/me");
         }
     }
 
+    /**
+     * Affiche la page de modification des informations personnelles.
+     * Si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion.
+     * Si la modification échoue, un message d'erreur est affiché.
+     * @return string
+     */
+
+    function modifyUser(): string
+    {
+        // Récupération de l'utilisateur connecté en SESSION.
+        $user = SessionHelpers::getConnected();
+
+        // Si l'utilisateur n'est pas connecté, on le redirige vers la page de connexion
+        if (!$user) {
+            return $this->redirect("/login");
+        }
+
+        // Gestion de la modification des informations personnelles
+        if (isset($_POST["email"]) && isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["telephone"])) {
+            $result = $this->emprunteur->modifyUser($_POST["email"], $_POST["nom"], $_POST["prenom"], $_POST["telephone"]);
+
+            // Si la modification est réussie, on redirige l'utilisateur vers sa page de profil
+            if ($result) {
+                $this->redirect("/me");
+            }
+        }
+
+        // Affichage de la page de modification des informations personnelles
+        return Template::render("views/user/modifyUser.php", array("user" => $user));
+    }
+
+    /**
+     * Affiche la page de téléchargement des informations personnelles.
+     * Si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion.
+     * @return string
+     */
+
+    function dlUser()
+    {
+        // Récupération de l'utilisateur connecté en SESSION.
+        $user = SessionHelpers::getConnected();
+
+        // On récupère les informations de l'utilisateur
+        $user = $this->emprunteur->dlUser($user->idemprunteur);
+
+        // On convertit les informations de l'utilisateur en JSON
+        $json = json_encode($user);
+
+        // On envoie le JSON à l'utilisateur, le fichier est nommé "informations_personnelles.json"
+        header('Content-disposition: attachment; filename=informations_personnelles.json');
+        header('Content-type: application/json');
+        echo $json;
+    }
 }
