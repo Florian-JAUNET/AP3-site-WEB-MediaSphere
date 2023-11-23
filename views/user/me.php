38b158877dc47f61af2eebb42b5b144df7564c89
flo_flooff <floflo.off@gmail.com>
2023-11-23 23:27:35 +0100

    Modifications et ajout sur le profil de l'utilisateur

--- views/user/me.php
+++ views/user/me.php
@@ -4,21 +4,30 @@
         <div class="w-full md:w-1/3 px-4">
             <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg px-6 py-4">
                 <div class="flex items-center justify-center mb-4">
-                    <img src="<?= \utils\Gravatar::get_gravatar($user->emailemprunteur) ?>" alt="Photo de profil"
-                         class="rounded-full h-32 w-32">
+                    <img src="<?= \utils\Gravatar::get_gravatar($user->emailemprunteur) ?>" alt="Photo de profil" class="rounded-full h-32 w-32">
                 </div>
                 <h1 class="text-3xl font-bold text-gray-900 mb-4">👋 <?= $user->prenomemprunteur ?></h1>
                 <div class="mb-4">
                     <h2 class="text-xl font-semibold text-gray-800 mb-2">Informations personnelles</h2>
-                    <p class="text-gray-600 mb-2"><span
-                                class="font-semibold">Email:</span> <?= $user->emailemprunteur ?></p>
-                    <p class="text-gray-600 mb-2"><span class="font-semibold">Nom:</span> <?= $user->nomemprunteur ?>
+                    <p class="text-gray-600 mb-2"><span class="font-semibold">Email :</span> <?= $user->emailemprunteur ?></p>
+                    <p class="text-gray-600 mb-2"><span class="font-semibold">Nom :</span> <?= $user->nomemprunteur ?>
                     </p>
-                    <p class="text-gray-600 mb-2"><span
-                                class="font-semibold">Prénom:</span> <?= $user->prenomemprunteur ?></p>
+                    <p class="text-gray-600 mb-2"><span class="font-semibold">Prénom :</span> <?= $user->prenomemprunteur ?></p>
                 </div>
 
                 <div class="p-5 text-center">
+                    <a class="bg-blue-600 text-white hover:bg-blue-900 font-bold py-3 px-6 rounded-full" href="/user/modifyUser">
+                        Modifier mes informations
+                    </a>
+                    <br>
+                    <br>
+                    <br>
+                    <a class="bg-green-600 text-white hover:bg-green-900 font-bold py-3 px-6 rounded-full" href="/user/dlUser">
+                        Télécharger mes informations
+                    </a>
+                    <br>
+                    <br>
+                    <br>
                     <a class="bg-red-600 text-white hover:bg-red-900 font-bold py-3 px-6 rounded-full" href="/logout">
                         Déconnexion
                     </a>
@@ -43,16 +52,29 @@
                     <?php foreach ($emprunts as $emprunt) { ?>
                         <div class="bg-white shadow-lg rounded-lg px-6 py-4">
                             <h2 class="text-xl font-semibold text-gray-800 mb-2"><?= $emprunt->titre ?></h2>
-                            <p class="text-gray-600 mb-2">Type: <span
-                                        class="font-semibold"><?= $emprunt->libellecategorie ?></span></p>
+                            <p class="text-gray-600 mb-2">Type : <span class="font-semibold"><?= $emprunt->libellecategorie ?></span></p>
                             <p class="text-gray-600 mb-2">
-                                Date d'emprunt:
+                                Date d'emprunt :
                                 <span class="font-semibold"><?= date_format(date_create($emprunt->datedebutemprunt), "d/m/Y") ?></span>
                             </p>
+                            <?php
+                            // Calcul le nombre de jours avant la date de retour
+                            $jours = date_diff(date_create($emprunt->dateretour), date_create(date("Y-m-d")))->format("%a");
+                            ?>
                             <p class="text-gray-600 mb-2">
-                                Date de retour prévue:
-                                <span class="font-semibold"><?= date_format(date_create($emprunt->dateretour), "d/m/Y") ?></span>
+                                Date de retour prévue :
+                                <span class="font-semibold"><?= date_format(date_create($emprunt->dateretour), "d/m/Y") . " ($jours)" ?></span>
                             </p>
+                            <?php
+                            // Si la date de retour est dépassée, on calcule le coût de la pénalité
+                            if (date_create($emprunt->dateretour) < date_create(date("Y-m-d"))) {
+                                $cout = (date_diff(date_create($emprunt->dateretour), date_create(date("Y-m-d")))->format("%a"));
+                            ?>
+                                <p class="text-white bg-red-600 rounded-full px-4 py-2">
+                                    <!-- on affiche dans le coin de la carte un petit texte blanc sur fond rouge -->
+                                    En retard ! Vous avez <?= $cout ?>€ de pénalité de retard !
+                                </p>
+                            <?php } ?>
                         </div>
                     <?php } ?>
                 </div>
--- views/user/signup.php
+++ views/user/signup.php
@@ -33,27 +33,32 @@
                 <form class="max-w-sm" method="post" action="/signup">
                     <div class="mb-4">
                         <label for="nom" class="block text-gray-800 font-semibold mb-2">Nom</label>
-                        <input type="text" id="nom" name="nom" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <input type="text" id="nom" name="nom" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" require>
                     </div>
 
                     <div class="mb-4">
                         <label for="prenom" class="block text-gray-800 font-semibold mb-2">Prénom</label>
-                        <input type="text" id="prenom" name="prenom" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <input type="text" id="prenom" name="prenom" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" require>
                     </div>
 
                     <div class="mb-4">
                         <label for="email" class="block text-gray-800 font-semibold mb-2">Email</label>
-                        <input type="email" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <input type="email" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" require>
                     </div>
 
                     <div class="mb-4">
                         <label for="telephone" class="block text-gray-800 font-semibold mb-2">Téléphone</label>
-                        <input type="tel" id="telephone" name="telephone" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <input type="tel" id="telephone" name="telephone" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" require>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="show-num-tel" class="block text-gray-800 font-semibold mb-2">Afficher mon numéro de téléphone</label>
+                        <input type="checkbox" id="show-num-tel" name="show-num-tel" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
                     </div>
 
                     <div class="mb-4">
                         <label for="password" class="block text-gray-800 font-semibold mb-2">Mot de passe</label>
-                        <input type="password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <input type="password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" require>
                     </div>
 
                     <div class="text-center">
