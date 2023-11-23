9ed0bb206e058863de86255073618401826ca2f6
flo_flooff <floflo.off@gmail.com>
2023-11-23 15:01:33 +0100

    Ajout de la page de téléchargement des informations de l'utilisateur connecté

---
+++ views/user/dlUser.php
@@ -0,0 +1,16 @@
+<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[calc(100vh-136px)] lg:py-0">
+    <div class="bg-white shadow-lg rounded-lg px-6 py-4">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8 text-center">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                Votre téléchargement va commencer !
+            </h1>
+            <p>Si le téléchargment ne commence pas dans quelques secondes, cliquez sur le bouton.</p>
+
+            <div class="text-center pt-8">
+                <a href="/user/dlUser" class="bg-indigo-600 text-white hover:bg-indigo-900 font-bold py-3 px-6 rounded-full">
+                    Télécharger
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
